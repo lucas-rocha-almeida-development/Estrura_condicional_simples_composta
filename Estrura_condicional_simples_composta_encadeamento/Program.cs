0bc4f9f1728b68b752691db3f1a0c52ec6056ab4
lucalmr <lucalmr12@gmail.com>
2023-06-12 18:57:23 -0300

    Adicionar arquivos de projeto.

---
+++ Estrura_condicional_simples_composta_encadeamento/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrura_condicional_simples_composta {
+internal class Program {
+    static void Main(string[] args) {
+        //estrutura condional composta
+        Console.WriteLine("Entre com um numero inteiro: ");
+        int NumeroInteiro = int.Parse(Console.ReadLine());
+
+        if (NumeroInteiro % 2 == 0) {
+            //Condição sendo verdadeira ira executar esse comando
+            Console.WriteLine("Numero Digitado é PAR!!");
+                
+        }
+        else {
+            //condição sendo falsa ira executar esse comando!!
+            Console.WriteLine("Numero Digitado é IMPAR!!");
+        }
+            Console.ReadKey();
+    }
+}
+}
